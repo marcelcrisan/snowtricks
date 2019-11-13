6e4b41314a50ed2c92ec78fd797becfd941d39db
marcel crisan <marcelcrisan@outlook.com>
2019-11-13 22:29:51 +0100

    i created delete trick button and controller #9

---
+++ src/Controller/DeleteTrickController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Form\TrickType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+
+class DeleteTrickController extends AbstractController
+{  
+    /**
+     * @Route("/trick/{id}/delete", name="trick_delete")
+    */
+
+    public function deleteTrick (Trick $trick, ObjectManager $manager)
+    {
+        if (!$trick) {
+            throw $this->createNotFoundException('Cette figure n\'existe pas !');
+        }
+    
+        $manager->remove($trick);
+        $manager->flush();
+    
+        return $this->render('front/front.html.twig');
+    }
+
+}
