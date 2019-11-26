90e55f6bf763da6c5d7f41e954359bc6e59ba553
marcel crisan <marcelcrisan@outlook.com>
2019-11-26 16:48:04 +0100

    nav ready to use, new javascript file and i fix route bug in homepage controller, fix bugs in style css #23

--- src/Controller/LogInController.php
+++ src/Controller/LogInController.php
@@ -5,30 +5,14 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 class LogInController extends AbstractController
 {
      /**
      * @Route("/connexion", name="security_login")
-     * @param \Symfony\Component\HttpFoundation\Response
      */
-    public function login():Response
+    public function login()
     {
-        $username = new User();
-
-        // if user not connected
-        if (!$username) return $this->json([
-            'code' => 403,
-            'message' => "Oups vous n'êtes pas encore inscrit"
-        ], 403);
-
-        $username = $username->getUsername();
-        return $this->json([
-            'code' => 200,
-            'message' => 'Bienvenue ' . $username . ' !',
-        ], 200);
-
-        //return $this->render('security/login.html.twig');
+        return $this->render('security/login.html.twig');
     }
 }
