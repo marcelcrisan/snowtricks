5ccf06b1e45d93265a816469be73f399b6393907
marcel crisan <marcelcrisan@outlook.com>
2020-02-18 20:31:18 +0100

    new features and upgrade to symfony 4.4

--- src/Migrations/Version20200218151001.php
+++ src/Migrations/Version20200218151001.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191202153850 extends AbstractMigration
+final class Version20200218151001 extends AbstractMigration
 {
     public function getDescription() : string
     {
